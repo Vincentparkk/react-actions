58180b25407fdf60bf5dd3a767374ead26701efa
hyovinpark <qkrgyqls46@naver.com>
2024-04-26 02:15:08 +0900

    first test

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Start from "./Start";
+import About from "./About";
+import Contact from "./Contact";
+
+function Homepage() {
+  return (
+    <>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="about">About</Link>
+          <Link to="contact">Contact</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default Homepage;
---
+++ src/pages/Start.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Start() {
+  return (
+    <>
+      <p>Start</p>
+    </>
+  );
+}
+
+export default Start;
