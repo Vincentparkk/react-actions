71946676e731cdd06832c4c356887f617126dbf2
hyovinpark <qkrgyqls46@naver.com>
2024-04-28 00:28:36 +0900

    eighteenth test

--- src/pages/Commontable.jsx
+++ src/pages/Commontable.jsx
@@ -7,7 +7,7 @@ export default function Commontable(props) {
   const [historyT, setHistoryT] = useState([]);
 
   useEffect(() => {
-    fetch("https://quill-wakeful-headline.glitch.me/applyItem")
+    fetch(`${process.env.REACT_APP_TODOS}/applyItem`)
       .then((res) => res.json())
       .then((data) => {
         setHistoryT(data); // 가정: 서버 응답의 첫 번째 항목이 필요한 데이터
--- src/setUpProxy.js
+++ src/setUpProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/v1",
     createProxyMiddleware({
-      target: "http://localhost:5174", // API 서버 주소
+      target: "https://quill-wakeful-headline.glitch.me", // API 서버 주소
       changeOrigin: true,
     })
   );
