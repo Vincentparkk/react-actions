9aabee2f9d66c07923b06d1ee490424e440fead2
hyovinpark <qkrgyqls46@naver.com>
2024-04-29 02:07:34 +0900

    test31

--- src/pages/Deduction_home.jsx
+++ src/pages/Deduction_home.jsx
@@ -135,7 +135,6 @@ export default function Deduction_home() {
 
       console.log("Data updated successfully");
       window.confirm("데이터가 모두 초기화되었습니다.");
-      window.location.reload();
     } catch (error) {
       console.error("Error:", error);
     }
