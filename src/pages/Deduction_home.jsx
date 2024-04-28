f2cab74ac5e05fd04e49f55953b95ebe809eda2e
hyovinpark <qkrgyqls46@naver.com>
2024-04-29 00:06:34 +0900

    test25

--- src/pages/Deduction_home.jsx
+++ src/pages/Deduction_home.jsx
@@ -32,15 +32,14 @@ export default function Deduction_home() {
 
   const [priceT, setPriceT] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("https://testdata1.vercel.app/applyItem")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setPriceT(data); // 가정: 서버 응답의 첫 번째 항목이 필요한 데이터
-  //     });
-  // }, []);
-
-  setPriceT(dummy.applyItem);
+  useEffect(() => {
+    // fetch("https://testdata1.vercel.app/applyItem")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setPriceT(data); // 가정: 서버 응답의 첫 번째 항목이 필요한 데이터
+    //   });
+    setPriceT(dummy.applyItem);
+  }, []);
 
   // console.log(priceT);
 
