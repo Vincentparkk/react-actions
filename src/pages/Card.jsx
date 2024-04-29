5a883ff39bb8f101df6ad330f6c81dea1551b51c
hyovinpark <qkrgyqls46@naver.com>
2024-04-29 23:30:18 +0900

    test40

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -5,25 +5,35 @@ import healthImage from "./healthimage.jpg";
 import styles from "./Card.module.scss";
 
 function Card() {
+  const name = "박산경";
   return (
     <>
-      <p>Card</p>
-      <div className={styles.wholePage}>
-        <div className={styles.movieContainer}>
-          <Link to="/deduction_home">
-            <img src={deductionImage} />
-            <div className={styles.moiveInfo}>
-              <h4>의료공제</h4>
-            </div>
-          </Link>
+      <div className={styles.page}>
+        <div className={styles.header}>
+          <div className={styles.header1}>
+            <p>안녕하세요,</p> <div className={styles.name}>{name}</div>
+            <p>님!</p>
+          </div>
+          <div>원하는 카드를 선택해주세요.</div>
         </div>
-        <div className={styles.movieContainer}>
-          <Link to="/health">
-            <img src={healthImage} />
-            <div className={styles.moiveInfo}>
-              <h4>보건실</h4>
-            </div>
-          </Link>
+
+        <div className={styles.wholePage}>
+          <div className={styles.movieContainer}>
+            <Link to="/deduction_home">
+              <img className={styles.image} src={deductionImage} />
+              <div className={styles.moiveInfo}>
+                <h3>의료공제</h3>
+              </div>
+            </Link>
+          </div>
+          <div className={styles.movieContainer}>
+            <Link to="/health">
+              <img className={styles.image} src={healthImage} />
+              <div className={styles.moiveInfo}>
+                <h3>보건실</h3>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </>
