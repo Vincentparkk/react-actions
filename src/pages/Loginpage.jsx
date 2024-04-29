ca9b697b31bd5a08c9a1ee2496da0c2fb69bc8e7
hyovinpark <qkrgyqls46@naver.com>
2024-04-29 19:13:30 +0900

    test35

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -1,27 +1,116 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import styles from "./Loginpage.module.scss";
 
 function Login() {
-  const [test1, setTest1] = useState([]);
+  const User = {
+    email: "ime.park@postech.ac.kr",
+    pw: "ime1234!",
+  };
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+
+  const [emailValid, setEmailValid] = useState(false);
+  const [pwValid, setPwValid] = useState(false);
+  const [notAllow, setNotAllow] = useState(true);
 
   useEffect(() => {
-    fetch("https://juniper-colossal-mail.glitch.me/personalInfo?id=8d97")
-      .then((res) => res.json())
-      .then((data) => {
-        setTest1(data); // 가정: 서버 응답의 첫 번째 항목이 필요한 데이터
-      });
-  }, []);
+    if (emailValid && pwValid) {
+      setNotAllow(false);
+      return;
+    }
+    setNotAllow(true);
+  }, [emailValid, pwValid]);
 
-  console.log(test1);
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    const regex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    if (regex.test(e.target.value)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
+  };
+  const handlePw = (e) => {
+    setPw(e.target.value);
+    const regex =
+      /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+    if (regex.test(e.target.value)) {
+      setPwValid(true);
+    } else {
+      setPwValid(false);
+    }
+  };
+  const onClickConfirmButton = () => {
+    if (email === User.email && pw === User.pw) {
+      alert("로그인에 성공했습니다.");
+    } else {
+      alert("등록되지 않은 회원입니다.");
+    }
+  };
 
   return (
     <>
-      <h1>Login</h1>
+      <div className={styles.page}>
+        <div className={styles.titleWrap}>
+          [ 로그인 ]
+          <br />
+          <br />
+          이메일과 비밀번호를
+          <br />
+          입력해주세요
+        </div>
 
-      <Link to="/card">
-        <button>로그인!</button>
-      </Link>
+        <div className={styles.contentWrap}>
+          <div className={styles.inputTitle}>이메일 주소</div>
+          <div className={styles.inputWrap}>
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="test@gmail.com"
+              value={email}
+              onChange={handleEmail}
+            />
+          </div>
+          <div className={styles.errorMessageWrap}>
+            {!emailValid && email.length > 0 && (
+              <div>올바른 이메일을 입력해주세요.</div>
+            )}
+          </div>
+
+          <div style={{ marginTop: "26px" }} className={styles.inputTitle}>
+            비밀번호
+          </div>
+          <div className={styles.inputWrap}>
+            <input
+              className={styles.input}
+              type="password"
+              placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+              value={pw}
+              onChange={handlePw}
+            />
+          </div>
+          <div className={styles.errorMessageWrap}>
+            {!pwValid && pw.length > 0 && (
+              <div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
+            )}
+          </div>
+        </div>
+
+        <div>
+          <Link to="/card">
+            <button
+              onClick={onClickConfirmButton}
+              disabled={notAllow}
+              className={styles.bottomButton}
+            >
+              확인
+            </button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
