c61d720b355f9e11d4d10208658cdf6ddeb65c72
hyovinpark <qkrgyqls46@naver.com>
2024-04-29 19:51:42 +0900

    test36

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import deductionImage from "./deductionimage.jpg";
+import healthImage from "./healthimage.jpg";
+import styles from "./Card.module.scss";
 
 function Card() {
   return (
     <>
       <p>Card</p>
-      <Link to="/deduction_home">Deduction_home</Link>
-      <Link to="/health">Health</Link>
+      <div className={styles.wholePage}>
+        <div className={styles.movieContainer}>
+          <Link to="/deduction_home">
+            <img src={deductionImage} />
+            <div className={styles.moiveInfo}>
+              <h4>의료공제</h4>
+            </div>
+          </Link>
+        </div>
+        <div className={styles.movieContainer}>
+          <Link to="/health">
+            <img src={healthImage} />
+            <div className={styles.moiveInfo}>
+              <h4>보건실</h4>
+            </div>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
