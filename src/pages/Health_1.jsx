18d411f93ec7a55bc07f7442d597eb3af2a66720
hyovinpark <qkrgyqls46@naver.com>
2024-05-02 01:55:16 +0900

    test53

--- src/pages/Health_1.jsx
+++ src/pages/Health_1.jsx
@@ -15,50 +15,53 @@ function Health_1() {
           <h1 className={styles.title}>보건실 운영 시간</h1>
         </div>
         <div className={styles.buttons}>
-          <Link to="/health/health_1">
-            <button className={styles.butt}>
-              <img
-                style={{ width: "50px", height: "70px" }}
-                src={image1}
-                alt="보건실 운영 시간"
-                className={styles.menuimg}
-              />
-              <h3 className={styles.buttP}>
-                보건실
-                <br />
-                운영 시간
-              </h3>
-            </button>
-          </Link>
-          <Link to="/health/health_2">
-            <button className={styles.butt}>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src={image2}
-                alt="보건실 구비 품목"
-                className={styles.menuimg}
-              />
-              <h3 className={styles.buttP}>
-                보건실
-                <br />
-                구비 품목
-              </h3>
-            </button>
-          </Link>
-          <Link to="/health/health_3">
-            <button className={styles.butt}>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src={image3}
-                alt="기타"
-                className={styles.menuimg}
-              />
-              <h3 className={styles.buttP}>기타</h3>
-            </button>
-          </Link>
+          <div className={styles.buttonArea}>
+            <Link to="/health/health_1">
+              <button className={styles.butt}>
+                <img
+                  src={image1}
+                  className={styles.menuimg}
+                  style={{ width: "30px", height: "38px" }}
+                />
+                <h3 className={styles.buttP}>
+                  보건실
+                  <br />
+                  운영 시간
+                </h3>
+              </button>
+            </Link>
+          </div>
+          <div className={styles.buttonArea}>
+            <Link to="/health/health_2">
+              <button className={styles.butt}>
+                <img
+                  src={image2}
+                  className={styles.menuimg}
+                  style={{ width: "35px", height: "35px" }}
+                />
+                <h3 className={styles.buttP}>
+                  보건실
+                  <br />
+                  구비 품목
+                </h3>
+              </button>
+            </Link>
+          </div>
+          <div className={styles.buttonArea}>
+            <Link to="/health/health_3">
+              <button className={styles.butt}>
+                <img
+                  src={image3}
+                  className={styles.menuimg}
+                  style={{ width: "35px", height: "35px" }}
+                />
+                <h3 className={styles.buttP}>기타</h3>
+              </button>
+            </Link>
+          </div>
         </div>
         <div className={styles.time}>
-          <img style={{ width: "450px", height: "550px" }} src={timetable} />
+          <img style={{ width: "90%", height: "50%" }} src={timetable} />
         </div>
       </div>
     </>
